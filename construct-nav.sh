2841344611ed90fb422e8063776012120beab7d3
David Jencks <djencks@apache.org>
2020-02-16 22:37:56 -0800

    set up as part of master@common

--- construct-nav.sh
+++ construct-nav.sh
@@ -2,4 +2,4 @@
 
 # construct a nav file listing all adocs in order.
 
-(cd tomee/modules/ROOT/pages;find . -name "*.adoc" -exec bash -c 'echo \* xref:${0//.\//}[]' {} \; |sort  > ../nav.adoc)
+(cd tomee/modules/ROOT/pages;find . -name "*.adoc" -exec bash -c 'echo \* xref:${0//.\//}[]' {} \; |sort  > ../nav0.adoc)
