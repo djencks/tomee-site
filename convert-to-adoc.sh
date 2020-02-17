a5a58e4da710c675f03c869855f95fd6c6e9e9c4
David Jencks <djencks@apache.org>
2020-02-16 17:21:45 -0800

    updated convert script for .md files

--- convert-to-adoc.sh
+++ convert-to-adoc.sh
@@ -7,4 +7,11 @@
 
 
 #(cd content; find . -name "*.mdtext" -type f -exec sh -c     'echo kramdoc --format=GFM --wrap=ventilate --output=../tomee/modules/ROOT/pages/${0//mdtext/adoc} $0' {} \;)
-(cd content; find . -name "*.mdtext" -type f -exec sh -c     'kramdoc --format=GFM --wrap=ventilate --output=../tomee/modules/ROOT/pages/${0//mdtext/adoc} $0' {} \;)
+# actually used for initial conversion.  However, I missed the .md files.
+#(cd content; find . -name "*.mdtext" -type f -exec sh -c     'kramdoc --format=GFM --wrap=ventilate --output=../tomee/modules/ROOT/pages/${0//mdtext/adoc} $0' {} \;)
+
+
+(cd content; find . -name "*.md" ! -path "*example*" -type f -exec sh -c     'kramdoc --format=GFM --wrap=ventilate --output=../tomee/modules/ROOT/pages/${0//md/adoc} $0' {} \;)
+
+
+(cd content; find . -name "*.md" -path "*example*" -type f -exec sh -c     'kramdoc --format=GFM --wrap=ventilate --output=../examples/modules/ROOT/pages/${0//md/adoc} $0' {} \;)
